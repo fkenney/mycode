38dac30c10abafdf4a9ddc819be454d8d6eb3b4b
fkenney <fkenney@users.noreply.github.com>
2022-09-20 15:14:34 +0000

    updated myscript with main

--- myscript.py
+++ myscript.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
-# ask for user name
-name = input("what is your name?:")
+def main():
+    # ask for user name
+    name = input("what is your name?:")
 
-# ask for day of the week
-day = input("what is the day of the week?:")
+    # ask for day of the week
+    day = input("what is the day of the week?:")
 
-# prints name and day of the week from user
-print(f"Hello, {name}! Happy {day}!")
+    # prints name and day of the week from user
+    print(f"Hello, {name}! Happy {day}!")
+
+if __name__ == "__main__": 
+    main()    
