e11a6a205d883e0f5b359118b77e25ed468620d5
fkenney <fkenney@users.noreply.github.com>
2022-09-20 17:49:34 +0000

    mixed listv2

--- mixlist.py
+++ mixlist.py
@@ -4,6 +4,9 @@ def main():
     #creats list of three items
     my_list = [ "192.168.0.5", 5060, "UP" ]
 
+    #creates list of IPs
+    iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1"]
+
     #prints first item in list
     print(f"The first item in the list (IP): {my_list[0]}")
     
@@ -12,6 +15,9 @@ def main():
 
     #prints last item in the list
     print(f"The last item in the list (state): {my_list[2]}")
+
+    #prints last two IPs
+    print(f"Last two IPs of iplist are {iplist[-2: ]} ")
 
 if __name__ == "__main__": 
         main()
